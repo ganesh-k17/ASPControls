a05f2f2f8afe2094022360d5506df64777fffc9e
ganeshk <ganku007@live.in>
2024-10-25 22:32:44 +0530

    feat:adds dropdown helpers

---
+++ ASPControls/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using ASPControls.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace ASPControls.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // load dropdown using enum
+            var categories = Enum.GetValues(typeof(Category))
+                .Cast<Category>()
+                .Select(c => new SelectListItem
+                {
+                    Value = ((int)c).ToString(),
+                    Text = c.ToString()
+                }).ToList();
+
+            // load dropdown using list object
+            var languages = new List<Language> {
+                new Language { Name= "French", code="fren"},
+                new Language { Name= "Spain", code="spain"},
+                new Language { Name= "Tamil", code="tam"},
+                new Language { Name= "German", code="germ"},
+                new Language { Name= "Telugu", code="tel"},
+            };
+
+
+            var studentNames = new List<string> { "Raja", "Vijay", "Parvati", "Viji" };
+
+
+            var model = new ProductViewModel { Categories = categories, CategoryId = 1, Languages = languages, SelectedLanguageCode = "germ", Category = Category.Electronics, Students = studentNames, selectedStudent="Parvati" };
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASPControls/Models/ProductViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ASPControls.Models
+{
+    public enum Category
+    {
+        Electronics = 1,
+        Books = 2,
+        Clothing = 3
+    }
+
+    public class Language
+    {
+        public string Name { get; set; }
+        public string code { get; set; }
+    }
+
+    public class ProductViewModel
+    {
+        public List<SelectListItem> Categories { get; set; }
+        public List<Language> Languages { get; set; }
+        public string SelectedLanguageCode { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+        public List<string> Students { get; set; }
+        public string selectedStudent { get; set; }
+    }
+}
